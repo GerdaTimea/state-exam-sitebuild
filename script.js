61a687f475d32a7de7fc7c7a32ecd12752dd087f
GerdaTimea <gerdatimi@gmail.com>
2022-03-23 16:26:44 +0100

    content added

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+function cardForCurrency(currency) {
+  return `
+    <section>
+      <article>
+        <h2 class="sub">${currency.sub}</h2>
+        <p class="title">${currency.title}</p>
+        <p class="text">${currency.text}</p>
+      </article>
+      <button>
+        <span class="button-text">Save</span>
+        <span class="material-icons">arrow_forward</span>
+      </button>
+    </section>
+  `;
+}
+
 function loadEvent() {
+  const mainElement = document.querySelector("main");
+
+  for (let i = 0; i < currencies.length; i++) {
+    const currency = currencies[i];
+    mainElement.insertAdjacentHTML("beforeend", cardForCurrency(currency));
+  }
 
 }
 
